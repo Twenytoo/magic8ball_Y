4edc4456b32899af85b420ab351b54a07c1163e5
twenytoo <alushan1997@gmail.com>
2021-10-16 16:28:43 +0300

    added Settings Controller with hardcoded answers

---
+++ MagicEightBall/Model/HardcodedAnswers.swift
@@ -0,0 +1,17 @@
+//
+//  HardcodedAnswers.swift
+//  MagicEightBall
+//
+//  Created by Артём on 14.10.2021.
+//
+
+import Foundation
+
+struct HardcodedAnswers {
+    
+    var answers: [String]
+    
+    
+    
+    
+}
--- MagicEightBall/View Controllers/SettingsViewController.swift
+++ MagicEightBall/View Controllers/SettingsViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,21 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return answers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let  answerCell = tableView.dequeueReusableCell(withIdentifier: "Answer", for: indexPath)
+        
+        answerCell.textLabel?.text = answers[indexPath.row]
+        
+        return answerCell
+        
+    }
 
 }
